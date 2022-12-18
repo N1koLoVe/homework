4be1a7c2f818c5ac5d354b39f24335cb5d3dfd84
N1koLoVe <119735349+N1koLoVe@users.noreply.github.com>
2022-12-18 01:03:42 +0100

    Array/Loops domasni
    6 domasni

--- app.js
+++ app.js
@@ -1,45 +1,99 @@
 console.log("it works");
 
 // Homework 1 
-function returnDiffType(parameter){
-    console.log(typeof parameter);
+
+function tellStory(name, mood, activity){
+    console.log(`This is ${name}. ${name} is a nice person. Today they are ${mood}. They ${activity} all day.The end.`);
 }
 
-returnDiffType({name: "Joce"});
-returnDiffType(true);
-returnDiffType(69);
-returnDiffType("jabuka");
-returnDiffType();
+tellStory("Joce", "Exhausted", "Played Football");
 
 // Homework 2
 
-function convertDogYears(years){
-    let dogYears = years * 7;
-    console.log( `Your dog is ${dogYears} years old in human years`);
+const numArr = [];
+
+function calculateSum (numArr){
+    let sum = 0;
+    for (i = 0; i < numArr.length; i++){
+    if(validateNumber(numArr[i])){
+        return `Theres is an invalid number in the array`;
+    }
+    sum += numArr[i];
+    }
+    return sum;
 }
 
-convertDogYears(7);
-
-function convertHumanYears(years){
-    let humanYears = parseInt(years / 7);
-    console.log( `You are ${humanYears} years old in dog years`);
+function validateNumber(num){
+    if (typeof num !== "number") 
+    return true;
 }
 
-convertHumanYears(81);
+console.log(calculateSum([1,2,3,4,5]));
+console.log(calculateSum([1,2,`3`,4,5]));
 
 // Homework 3
 
-let balance = 20000;
+const words = ["Hello", "there", "students", "of", "SEDC", "!"];
 
-function withdrawMoney(){
-    const withdraw = prompt("How much would you like to withdraw?");
-    if (withdraw > balance){
-        console.log(`You dont have enough money in your bank account. You have $ ${balance} left in your bank account`);
-        return;
-    }
-    balance -= withdraw;
-    console.log(`You withdrew $ ${withdraw}. You have $ ${balance} left in your bank account`);
+function createSentence(words){
+    let sentence = "";
+    for (i = 0; i < words.length; i++){
+        sentence += words[i] + " ";
+}
+    return sentence;
 }
 
-withdrawMoney();
-withdrawMoney();
+console.log(createSentence(words));
+
+// Homework 4
+
+
+function oddOrEven (){
+    for (let i = 1; i <= 20; i++){
+        if (i % 2){ 
+            console.log(`${i} is an odd number`);
+        }
+        else{
+            console.log(`${i} is an even number`);
+        }
+    }   
+}
+
+oddOrEven();
+
+
+// Homework 5 
+
+const arr = [3,5,6,8,11];
+let min = arr[0];
+let max = 0;
+let sum = 0;
+
+function minMaxSum(){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] < min){
+        min = arr[i];
+        }
+        if(arr[i] > max){
+        max = arr[i];
+        }
+        sum = max + min;
+    }
+}
+
+minMaxSum();
+console.log(max,min,sum);
+
+
+// Homework 6
+
+const firstName = ["Lana", "Johnny"];
+const lastName = ["Rhoades", "Sins"];
+
+function fullName(){
+    for(let i = 0; i < firstName.length; i++){ 
+    console.log(`${i + 1} ${firstName[i]} ${lastName[i]}`);
+    }
+}
+
+fullName();
